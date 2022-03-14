d5ff6f423741b6a595a895f1a7ba7b06fefc8418
Sergey Indenbom <dzorogh@gmail.com>
2022-03-14 18:23:20 +0300

    Get current folder

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import AppLayout from "@/components/AppLayout.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import { settings } from "./setup";
+import api from "@/services/api";
+import { useNodesStore } from "@/store/nodes";
+
 
 const routes = [
   {
@@ -28,6 +31,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(settings.basePath),
   routes,
+});
+
+
+router.afterEach(async (to, from) => {
+  const nodesStore = useNodesStore();
+
+  if (to.name === 'folder') {
+    nodesStore.currentFolder = await api.getFolder({ id: to.params.folderId as string });
+  }
+  if (to.name === 'root') {
+    nodesStore.currentFolder = null;
+  }
 });
 
 export default router;
