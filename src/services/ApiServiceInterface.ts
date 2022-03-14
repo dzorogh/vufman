af6a9c885f56692abae066f52938a9dbe29ac550
Sergey Indenbom <dzorogh@gmail.com>
2022-03-14 18:25:26 +0300

    Remove getNavigator and add getFolder

--- src/services/ApiServiceInterface.ts
+++ src/services/ApiServiceInterface.ts
@@ -1,10 +1,10 @@
 import { Node } from '@/types/Node';
 import { NodesRequest } from '@/types/NodesRequest';
+import { FolderRequest } from "@/types/FolderRequest";
 
 export interface ApiServiceInterface {
 
-  getNavigator: () => Promise<Node[]>;
-
   getNodes: (request: NodesRequest) => Promise<Node[] | []>;
 
+  getFolder: (request: FolderRequest) => Promise<Node | null>;
 }
