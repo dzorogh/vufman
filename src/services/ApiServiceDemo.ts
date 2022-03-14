49b4cc3b06f01d24349ce78b2364f3864401046c
Sergey Indenbom <dzorogh@gmail.com>
2022-03-14 18:24:40 +0300

    Add timout and folder data

--- src/services/ApiServiceDemo.ts
+++ src/services/ApiServiceDemo.ts
@@ -1,17 +1,50 @@
 import axios from 'axios';
 import { ApiServiceInterface } from './ApiServiceInterface';
+import { NodesRequest } from "@/types/NodesRequest";
+import { FolderRequest } from "@/types/FolderRequest";
+import { Node } from "@/types/Node";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { promiseTimeout } from '@vueuse/core';
 
 export default class ApiServiceDemo implements ApiServiceInterface {
 
-  async getNavigator() {
-    const result = await axios.get('/data/navigator.json');
+  async getNodes(request: NodesRequest) {
+    const nodes = await axios.get('/data/nodes.json');
+    await promiseTimeout(Math.random() * 2000 + 100);
 
-    return result.data.data;
+    let data = nodes.data.data as Node[];
+
+    if (!data) {
+      return [];
+    }
+
+    if (request.isFolder !== undefined) {
+      data = data.filter((node) => node.isFolder === request.isFolder);
+    }
+
+    if (request.folderId !== undefined) {
+      data = data.filter((node) => node.folderId === request.folderId);
+    }
+
+    return data;
+    // todo: how to get trash
   }
 
-  async getNodes() {
-    const result = await axios.get('/data/nodes.json');
-    return result.data.data;
+  async getFolder(request: FolderRequest) {
+    const nodes = await axios.get('/data/nodes.json');
+    await promiseTimeout(Math.random() * 2000 + 100);
+
+    const data = nodes.data.data as Node[];
+
+    const filtered = data.filter((node) => node.id === request.id);
+
+    if (filtered.length) {
+      return filtered[0];
+    }
+
+    return null;
   }
 
 }
