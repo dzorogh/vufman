4fd624c3855fb9a0baab48e4373e4c7ac4452f01
Sergey Indenbom <dzorogh@gmail.com>
2022-03-14 13:31:51 +0300

    Refactoring

--- src/components/AppFolderView.vue
+++ src/components/AppFolderView.vue
@@ -41,40 +41,6 @@ const nodesStore = useNodesStore();
 
 nodesStore.nodes = await Api.getNodes({ id: null });
 
-// const selectedNodes = ref<Node[]>([]);
-// const selectNodeSingle = (node: Node) => {
-//   selectedNodes.value = [ node ];
-// };
-// const selectNodeControl = (node: Node) => {
-//   selectedNodes.value.push(node);
-// };
-// const selectNodeShift = (node: Node) => {
-//   if (selectedNodes.value.length <= 0) {
-//     selectedNodes.value = [ node ];
-//   } else {
-//     if (nodes.value) {
-//
-//       const firstNodeIndex = nodes.value.indexOf(selectedNodes.value[0]);
-//       const lastNodeIndex = firstNodeIndex + selectedNodes.value.length - 1;
-//       const selectedNodeIndex = nodes.value.indexOf(node);
-//
-//       selectedNodes.value = [];
-//
-//       // Select all nodes between first and selected node
-//       // Including edge items
-//       nodes.value.forEach((item, index) => {
-//         if (index >= firstNodeIndex && index <= selectedNodeIndex) {
-//           selectedNodes.value.push(item);
-//         }
-//
-//         if (index >= selectedNodeIndex && index <= lastNodeIndex) {
-//           selectedNodes.value.push(item);
-//         }
-//       });
-//     }
-//   }
-// };
-
 const menu = ref();
 const showContextMenu = (node: Node, event: unknown) => {
   if (!nodesStore.isNodeSelected(node)) {
