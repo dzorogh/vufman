ddb07feb65bf71936eed164e79ff51043da62b0d
Sergey Indenbom <dzorogh@gmail.com>
2022-03-14 18:17:27 +0300

    Making breadcrumbs

--- src/components/AppBreadCrumbs.vue
+++ src/components/AppBreadCrumbs.vue
@@ -1,8 +1,51 @@
 <template>
-  Корневая папка
+  <div class="">
+    <div
+      v-if="nodesStore.currentFolder"
+      class="flex gap-4 items-center"
+    >
+      <Button
+        class=" p-button-text"
+        icon="pi pi-arrow-left"
+        @click="handleClickBack"
+      />
+
+      <div class="font-bold opacity-50">
+        {{ nodesStore.currentFolder.name }}
+      </div>
+    </div>
+    <div v-else>
+      <div class="font-bold opacity-50 px-4">
+        Диск
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { useNodesStore } from "@/store/nodes";
+import Button from "primevue/button";
+import { useRouter } from "vue-router";
+
+const nodesStore = useNodesStore();
+const router = useRouter();
+
+const handleClickBack = function () {
+  if (nodesStore.currentFolder) {
+    if (nodesStore.currentFolder.folderId) {
+      router.push({
+        name: 'folder',
+        params: {
+          folderId: nodesStore.currentFolder.folderId
+        }
+      });
+    } else {
+      router.push({
+        name: 'root',
+      });
+    }
+  }
+};
 
 </script>
 
