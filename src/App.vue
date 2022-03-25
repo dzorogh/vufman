1a38e16a9034d73def0ba48a6c7e80049a05576c
Sergey Indenbom <dzorogh@gmail.com>
2022-03-25 17:39:33 +0300

    Move theme to settings file, generated by theme editor

--- src/App.vue
+++ src/App.vue
@@ -21,15 +21,16 @@
 <script setup lang="ts">
 import DialogConfirm from "./components/DialogConfirm.vue";
 import DialogPrompt from "@/components/DialogPrompt.vue";
-import { GlobalThemeOverrides } from "naive-ui";
+// import { GlobalThemeOverrides } from "naive-ui";
+import themeOverrides from "./naive-ui-theme-overrides.json";
 
-const themeOverrides: GlobalThemeOverrides = {
-  "common": {
-    "primaryColor": "#2a86daFF",
-    "primaryColorHover": "#50A2EDFF",
-    "primaryColorPressed": "#1D72C1FF",
-    "primaryColorSuppl": "#165691FF"
-  }
-};
+// const themeOverrides: GlobalThemeOverrides = {
+//   "common": {
+//     "primaryColor": "#2a86daFF",
+//     "primaryColorHover": "#50A2EDFF",
+//     "primaryColorPressed": "#1D72C1FF",
+//     "primaryColorSuppl": "#165691FF"
+//   }
+// };
 
 </script>
