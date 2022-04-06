2fe457bd849f99007532aa4b0fd3436621b2567e
Sergey Indenbom <dzorogh@gmail.com>
2022-04-06 19:41:49 +0300

    Fix node type

--- src/types/INode.ts
+++ src/types/INode.ts
@@ -3,17 +3,20 @@ import { IAccess } from "@/types/IAccess";
 export interface INode {
   // All nodes
   ancestors: INode[];
-  isTrashed: boolean;
-  id: string;
+  id: string | number;
   name: string;
   isFolder: boolean;
   size: number;
   comment?: string;
-  folderId: string | null;
+  folderId: INode['id'] | null;
   createdAt: ReturnType<Date['toISOString']> | null;
   updatedAt: ReturnType<Date['toISOString']> | null;
   deletedAt: ReturnType<Date['toISOString']> | null;
   trashedAt: ReturnType<Date['toISOString']> | null;
+
+  isTrashed: boolean;
+  isDeleted: boolean;
+
   authorId: number | string;
   author?: {
     firstName: string;
